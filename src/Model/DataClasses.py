0ee994a950b4c040be12170a608401d94365424a
mazucanti <daniel.mazucanti.ismart@gmail.com>
2025-09-15 11:51:09 -0300

    Added pdv dbscan clustering to the classes

--- src/Model/DataClasses.py
+++ src/Model/DataClasses.py
@@ -1,16 +1,16 @@
-
+import polars as pl
 
 class GenericData:
     dataframe
-    filepath = str
-    encoding = str
+    filepath: str
+    encoding: str
     
-    def __init__(self, filepath, encoding):
+    def __init__(self, filepath):
         self.filepath = filepath
         self.encoding = encoding
 
-    def read_file(self):
-        pass
+    def read_file(self, filepath):
+        return pl.scan_parquet(filepath)
     
     def write_file(self, path):
         pass
@@ -25,7 +25,38 @@ class SKU(GenericData):
     pass
 
 class PDV(GenericData):
-    pass
+
+    def __init__(self, filepath, encoding):
+        super.__init__(filepath, encoding)
+        dataframe = self.read_file(self.filepath)
+
+
+    def _prepare_pdv(pdv: pl.LazyFrame, transactions: pl.LazyFrame) -> pl.DataFrame:
+        return dataframe.with_columns(
+            pl.col('zipcode').cast(pl.String).name.keep(),
+            premise_dummy=pl.when(pl.col('premise').str.contains('On')).then(1).otherwise(0),
+        ).with_columns(
+            pl.when(pl.col('zipcode') == '8107').then(pl.lit('08107')).otherwise('zipcode').name.keep(),
+        ).join(
+            transactions,
+            left_on='pdv',
+            right_on='internal_store_id',
+            how='inner',
+            validate="1:m"
+        ).drop(
+            'transaction_date',
+            'reference_date',
+            'premise',
+        ).group_by('pdv').agg(
+            pl.col('quantity', 'gross_value', 'net_value', 'gross_profit', 'discount', 'taxes').mean(),
+            pl.col('zipcode', 'categoria_pdv').implode().first(),
+            pl.col('distributor_id').n_unique().alias('distributor_count'),
+            pl.col('gross_profit').len().alias('transaction_count'),
+            pl.col('internal_product_id').implode().alias('transacted_products'),
+        ).collect()
+
+    def data_cleanup(self):
+        return _prepare_pdv()
 
 class Transactions(GenericData):
     pass
--- src/Model/MLClasses.py
+++ src/Model/MLClasses.py
@@ -1,7 +1,10 @@
 from DataClasses import GenericData
+from sklearn.cluster import DBSCAN
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import PowerTransformer, OrdinalEncoder, StandardScaler
 
 class MLModel:
-    input_data = GenericData
+    input_data: GenericData
     output_data
 
     def __init__(self):
@@ -9,11 +12,14 @@ class MLModel:
 
     def preproc(self):
         pass
-
+    
+    def param_search(self):
+        pass
+    
     def run_model(self):
         pass
 
-    def set_input_data(self, input_data):
+    def set_input_data(self, input_data: GenericData):
         self.input_data = input_data
 
     def generate_output(self):
@@ -22,3 +28,83 @@ class MLModel:
     def get_output_data(self):
         return self.output_data
 
+class DBSCANPDV(MLModel):
+
+    def __init__(self, pdvs: GenericData):
+        super.__init__()
+        self.set_input_data(pdvs)
+
+    def preproc(self):
+        non_scaled_pdv = self.input_data.data_cleanup()
+        transformed_pdv = _apply_transformers_for_dbscan(non_scaled_pdv)
+
+    def eps_search(self):
+        
+        with pl.Config(set_float_precision=1):
+            for eps in np.arange(0.1, 2, 0.2):
+                cluster_metrics_pdv(
+                    pdv_with_cluster=train_dbscan_pdv(
+                        transformed_pdv, eps=eps, min_samples=100
+                    ),
+                    non_scaled_df=non_scaled_pdv,
+                    eps=eps
+                )
+
+    def _apply_transformers_for_dbscan(self) -> pl.DataFrame:
+
+        transformer1 = ColumnTransformer(
+            transformers=[('ord', OrdinalEncoder(), ['zipcode'])],
+            remainder='passthrough',
+            verbose_feature_names_out=False
+        )
+        transformer1.set_output(transform='polars')
+        columns_scale = [coluna for coluna in self.input_data.columns if coluna not in ['pdv', 'zipcode']]
+        transformer2 = ColumnTransformer(
+            transformers=[
+                ('scaled', StandardScaler(), ['zipcode']),
+                ('power_transformed', PowerTransformer(), columns_scale)
+            ],
+            remainder='passthrough',
+            verbose_feature_names_out=False
+        )
+        transformer2.set_output(transform='polars')
+        return transformer2.fit_transform(
+            transformer1.fit_transform(self.input_data)
+        )
+
+    def run_model(self):
+        return train_dbscan_pdv(transformed_pdv, eps=0.9, min_samples=100)
+    
+    def train_dbscan_pdv(self, X_transformed: pl.DataFrame, eps: np.floating, min_samples: int) -> pl.DataFrame:
+        numpy_X = X_transformed.drop('pdv').to_numpy()
+        dbscan = DBSCAN(eps=eps, min_samples=min_samples, n_jobs=-1)
+        return pl.concat([
+            X_transformed,
+            pl.DataFrame({'cluster':dbscan.fit_predict(numpy_X, sample_weight=None)})
+            ], how='horizontal'
+        )
+
+    def cluster_metrics_pdv(pdv_with_cluster: pl.DataFrame, non_scaled_df: pl.DataFrame, eps: np.floating):
+        print(f''' Com eps {eps}, temos:
+                {
+                    pdv_with_cluster
+                    .select('pdv', 'cluster')
+                    .join(non_scaled_df, on='pdv', validate='1:1', maintain_order='right', how='full', suffix='_original')
+                    .drop('pdv')
+                    .group_by('cluster').agg(
+                        (~cs.by_name('pdv_original', 'zipcode')).mean(),
+                        pl.col('zipcode').mode().last(),
+                        pl.col('pdv_original').len().alias('cluster_count')
+                    )
+                    .sort('cluster', descending=False)
+                    .with_columns(
+                        (pl.col('quantity')*pl.col('transaction_count')).alias('total_quantity')
+                    )
+                    .with_columns(
+                        (pl.col('gross_value')*pl.col('total_quantity')).alias('total_gross_value')
+                    )
+                }\n\n
+                '''
+        )
+
+    
