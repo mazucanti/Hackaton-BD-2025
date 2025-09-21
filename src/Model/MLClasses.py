22a64ba46ed2492a5f045df039c4b560ee7465f6
mazucanti <daniel.mazucanti.ismart@gmail.com>
2025-09-21 18:33:22 -0300

    added thompson sampling

--- src/Model/MLClasses.py
+++ src/Model/MLClasses.py
@@ -116,3 +116,50 @@ class DBSCANPDV(MLModel):
         )
 
     
+class ThompsonSampling():
+
+    transactions: pl.LazyFrame
+    stores: pl.LazyFrame
+
+    def __init__(self, trans_path, store_path):
+        self.transactions = pl.scan_parquet(trans_path)
+        self.stores = pl.scan_parquet(store_path)
+
+    def _get_sample_list(self, ):
+        self.samples = [np.random.beta(s+1, l+1) for s, l in self.succ_loss]
+
+    def _update_best_sku(self):
+
+        if np.random.uniform() < self.probabilities[self.best_sku]:
+            self.succ_loss[best_sku][0] += 1
+        else:
+            self.succ_loss[best_sku][1] += 1
+        
+    def _get_best_sku(self):
+        self.best_sku = np.argmax(samples)
+
+    def preproc(self) -> pl.LazyFrame:
+        return self.stores.join(
+            self.transactions, how='inner', left_on='pdv', right_on='internal_store_id'
+            ).select(
+                'pdv',
+                'categoria_pdv',
+                'transaction_date',
+                'quantity',
+                'internal_product_id'
+            ).filter(
+                pl.col('quantity') > 0
+            ).with_columns(
+                pl.col('transaction_date').dt.week().alias('week'),
+            ).group_by(
+                ['pdv', 'week', 'internal_product_id']
+            ).agg(
+                pl.col('quantity').sum(),
+                pl.col('categoria_pdv').first()
+            ).group_by(
+                ['categoria_pdv', 'internal_product_id', 'week']
+            ).agg(
+                pl.col('quantity').mean()
+            ).sort('week')
+
+    
